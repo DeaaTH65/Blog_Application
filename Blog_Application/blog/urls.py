91c7c23dcc402303821d6390cc405dd54814eefc
Deaath65 <deaath65@gmail.com>
2023-06-21 18:24:08 +0545

    update post

--- Blog_Application/blog/urls.py
+++ Blog_Application/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView
 
 
 # using class based view.
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name='add-post'),
+    path('article/edit/<pk>', UpdatePostView.as_view(), name='update-post'),
 ]
