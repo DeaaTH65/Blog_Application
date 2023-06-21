9b29c55ba230063ea284d77b61a66cf01e526f42
Deaath65 <deaath65@gmail.com>
2023-06-21 15:08:36 +0545

    post added in website

--- Blog_Application/blog/urls.py
+++ Blog_Application/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView
+from .views import HomeView, ArticleDetailView, AddPostView
 
 
 # using class based view.
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add-post'),
 ]
