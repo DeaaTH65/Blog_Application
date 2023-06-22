32b8e9a03b4cbe2d3affdc937ff1ab406beff979
Deaath65 <deaath65@gmail.com>
2023-06-22 17:43:37 +0545

    category links in navbar

--- Blog_Application/blog/views.py
+++ Blog_Application/blog/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Category
@@ -13,6 +14,12 @@ class HomeView(ListView):
     # ordering = ['-id']
     ordering = ['-post_date']
     
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
+
     
 def CategoryView(request, cat):
     category_posts = Post.objects.filter(category=cat)
@@ -22,6 +29,12 @@ def CategoryView(request, cat):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        context['cat_menu'] = cat_menu
+        return context
     
     
 class AddCategoryView(CreateView):
