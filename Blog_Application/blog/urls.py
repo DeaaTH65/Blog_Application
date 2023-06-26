e8d6646d0e74785a49b8fd11695dc011c4590057
Deaath65 <deaath65@gmail.com>
2023-06-26 11:15:00 +0545

    comments in web page

--- Blog_Application/blog/urls.py
+++ Blog_Application/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, LikeView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, LikeView, AddCommentView
 
 
 # using class based view.
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('article/<int:pk>/comment/', AddCommentView.as_view(), name='add-comment'),
     path('add_post/', AddPostView.as_view(), name='add-post'),
     path('add_category/', AddCategoryView.as_view(), name='add-category'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update-post'),
